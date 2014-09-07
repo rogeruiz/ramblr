cfe06feb1376e72164cab1fb9b8cbfff542765c0
Roger Steve Ruiz <hi@rog.gr>
2014-09-06 23:21:22 -0400

    Add simple 404 redirect

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('tag', { path: '/tag/:tag_id' });
   this.resource('place', { path: '/place/:place_id' });
+
+  this.route('404', { path: "/*path" });
 });
 
 export default Router;
