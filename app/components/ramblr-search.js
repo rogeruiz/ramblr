be70208050042257a4f28f581092564ba3e06c23
Roger Steve Ruiz <hi@rog.gr>
2014-09-08 23:49:17 -0400

    Update the ramblr-search component
    This patch adds Sass support and Font Awesome styles to the project.
    The Powered By Google logos are also added since they're required to
    the Google Places API.
    
    I also cleaned up some cruft in the application template.

--- app/components/ramblr-search.js
+++ app/components/ramblr-search.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['ramblr-search', 'js-search'],
+
   value: null,
   isTag: null,
+  showResults: false,
+
+  suggestedPlaces: function() {
+    return [];
+  }.property(),
 
   getValue: function() {
     var value = this.get('value');
@@ -16,11 +23,38 @@ export default Ember.Component.extend({
     }
   }.observes('value'),
 
+  autocomplete: function() {
+    var component = this;
+    if (this.get('value').length >= 3 && !this.get('isTag')) {
+      var value = this.getValue().toLowerCase();
+
+      if (/.*near\.*/ig.test(value)) {
+        component.google.getPlaceByQuery(value).then(function(response) {
+          component.set('suggestedPlaces', response);
+        });
+      } else {
+        component.google.getPlace(value).then(function(response) {
+          component.set('suggestedPlaces', response);
+        });
+      }
+
+    } else {
+      component.set('suggestedPlaces', []);
+    }
+  }.observes('value', 'isTag'),
+
   actions: {
+    showResults: function() {
+      this.set('showResults', true);
+    },
+    hideResults: function() {
+      this.set('showResults', false);
+    },
     submit: function() {
       var route = this.get('isTag') ? 'tag' : 'place';
       var value = this.getValue().toLowerCase();
       this.sendAction('search', route, value);
+      this.$('input').blur();
     }
   }
 });
