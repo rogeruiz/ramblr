43e422ad4daa3ecfe290e8811b5db893298b49d0
Roger Steve Ruiz <hi@rog.gr>
2014-09-08 23:45:52 -0400

    Add Font Awesome to the project

--- Brocfile.js
+++ Brocfile.js
@@ -4,17 +4,19 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
+var pickFiles = require('broccoli-static-compiler');
 
-module.exports = app.toTree();
+// Font Awesome Setup
+var fontAwesomeFontTree = pickFiles('vendor/font-awesome/fonts', {
+  srcDir: '/',
+  files: [
+    'fontawesome-webfont.eot',
+    'fontawesome-webfont.ttf',
+    'fontawesome-webfont.svg',
+    'fontawesome-webfont.woff'
+  ],
+  destDir: '/assets/fonts'
+});
+
+var mergeTrees = require('broccoli-merge-trees');
+module.exports = mergeTrees([app.toTree(), fontAwesomeFontTree]);
