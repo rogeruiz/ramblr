ea341a42c83a4388bfc831b6ea3fa6dbd2e0629e
Roger Steve Ruiz <hi@rog.gr>
2014-09-08 23:45:31 -0400

    Rename project to ramblr

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ramblr-ember
+# Ramblr
 
 This README outlines the details of collaborating on this Ember application.
 
--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ import loadInitializers from 'ember/load-initializers';
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: 'ramblr-ember', // TODO: loaded via config
+  modulePrefix: 'ramblr', // TODO: loaded via config
   Resolver: Resolver
 });
 
-loadInitializers(App, 'ramblr-ember');
+loadInitializers(App, 'ramblr');
 
 export default App;
